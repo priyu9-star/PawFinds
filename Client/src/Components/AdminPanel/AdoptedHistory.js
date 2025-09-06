296aeada103b5ee1564336d2fadd027d6bbc049d
Priyanshi Jayant <22cd10pr783@mitsgwl.ac.in>
2025-09-06 22:59:46 +0530

    Update AdoptedHistory.js

--- Client/src/Components/AdminPanel/AdoptedHistory.js
+++ Client/src/Components/AdminPanel/AdoptedHistory.js
@@ -7,7 +7,7 @@ const AdoptedHistory = () => {
 
   const fetchAdoptedPets = async () => {
     try {
-      const response = await fetch('http://localhost:4000/adoptedPets');
+      const response = await fetch('srv-d2u6o77diees73clhk5g/adoptedPets');
       if (!response.ok) {
         throw new Error('An error occurred while fetching adopted pets');
       }
